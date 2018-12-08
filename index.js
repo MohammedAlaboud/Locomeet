1c90f48d5b8bcd8e3c2162ca317d55c74fa35e45
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2018-12-08 16:13:21 +0000

    redo

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var http = require('http');
-var url = require('url');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	q=url.parse(req.url, true);
-	query=q.query;
-	pathname=q.pathname;
-	switch(pathname){
-		case "/create":
-			res.write("Page to create new form");
-			break;
-		case "/input":
-			res.write("Page to input data");
-			break;			
-		case "/results":
-			res.write("Page with results");
-			token=query.i;
-			var algo = require('algo');
-			algo.getMeetingTimes()
-			break;
-		default:
-			res.write("hm...nothing here");
-			return;
-	}
-	res.end();
-}).listen(8080);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var controller = require('./controllers/user.controller');
+//require modules to use their methods through created variables.
+
+var port = 8080;
+//var for database run locally through MongoDB. Must run mongo on terminal to use first
+
+app.use(express.static(path.join(__dirname, 'views')));
+//connect to files in views folder
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//routes 
+app.get('/', function(req, res) { //route to get index.html file
+  res.render('index.html'); //automatically looks for and in a "views" directory 
+});
+
+
+//server setup
+app.listen(port, function() {
+  console.log('app listening on port ' + port);
+});
