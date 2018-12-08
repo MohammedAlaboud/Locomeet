002846619e57b4db709b1b02e978578e82541273
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2018-12-08 14:34:13 +0000

    Added Firebase Credentials

--- LocoMeet/src/app/firebase.credentials.ts
+++ LocoMeet/src/app/firebase.credentials.ts
@@ -1,5 +1,10 @@
 
 
 export const FIREBASE_CONFIG = {
-
-}
+    apiKey: "AIzaSyCdA1dGq2BBdStP1ptvFeS_pgjIgu4c-Ao",
+    authDomain: "locomeet-ff5ea.firebaseapp.com",
+    databaseURL: "https://locomeet-ff5ea.firebaseio.com",
+    projectId: "locomeet-ff5ea",
+    storageBucket: "locomeet-ff5ea.appspot.com",
+    messagingSenderId: "672562812201"
+  };
